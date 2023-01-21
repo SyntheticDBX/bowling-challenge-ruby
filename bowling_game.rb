6e43de513fd7b3075634c7a4c37d6022a3fef139
SyntheticDBX <ry.mchugh@outlook.com>
2023-01-21 13:01:32 +0000

    added class for game logic

--- bowling_game.rb
+++ bowling_game.rb
@@ -19,3 +19,36 @@ class Frame
         @rolls[0] + @rolls[1] == 10
     end
 end
+
+class Game
+    def initialize
+        @frames = []
+    end
+
+    def roll(pins)
+        if @frames.length == 0
+            @frames.push(Frame.new)
+        end
+        if @frames.last.is_strike? || @frames.last.rolls.length == 2
+            @frames.push(Frame.new)
+        end
+        @frames.last.roll(pins)
+    end
+
+    def score
+        @frames.each_with_index.reduce(0) do |score, (frame, index)|
+            score += frame.score
+            if frame.is_strike?
+                score += @frames[index + 1].rolls[0]
+                score += @frames[index + 1].rolls[1]
+                if @frames[index + 1].is_strike?
+                    score += @frames[index + 2].rolls[0]
+                end
+            elsif frame.is_spare?
+                score += @frames[index + 1].rolls[0]
+            end
+            score
+        end
+    end
+end
+
